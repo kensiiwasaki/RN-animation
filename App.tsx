bf84d33550620e479da64a943d5db81f7a3c9009
kensiiwasaki <kensiiwasaki@gmail.com>
2023-05-02 16:13:25 +0900

    first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Lottie from "lottie-react-native"
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Lottie
+      source={require('./assets/gradientBall.json')}
+      autoPlay
+      loop
+      />
       <StatusBar style="auto" />
     </View>
   );
