5e36ca8ec044dc7ffb5eea1d6734f1a82756ab34
kensiiwasaki <kensiiwasaki@gmail.com>
2023-05-08 03:06:29 +0900

    サンプルUI

--- App.tsx
+++ App.tsx
@@ -1,55 +1,119 @@
-import { useRef } from 'react';
+import React, { useRef } from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
   View,
+  Image,
   Animated,
-  Button,
-  Easing,
   ScrollView,
+  TouchableOpacity,
 } from 'react-native';
 
-export default function App() {
-  const animatedScrollY = useRef(new Animated.Value(0)).current;
+const MAX_HEADER_HEIGHT = 150;
+const MIN_HEADER_HEIGHT = 80;
+const MAX_AVATAR_SIZE = 100;
+const AVATAR_TOP = MAX_HEADER_HEIGHT - MAX_AVATAR_SIZE / 2;
 
+export default function App() {
   return (
-    <ScrollView
-      onScroll={Animated.event([
-        {
-          nativeEvent: {
-            contentOffset: {
-              y: animatedScrollY,
-            },
+    <View style={styles.container}>
+      <ScrollView>
+        <View style={{ height: MAX_HEADER_HEIGHT * 1.5 }} />
+        <View style={styles.item} />
+        <View style={styles.item} />
+        <View style={styles.item} />
+        <View style={styles.item} />
+        <View style={styles.item} />
+        <View style={styles.item} />
+        <View style={styles.item} />
+        <View style={styles.item} />
+        <View style={styles.item} />
+        <View style={styles.item} />
+        <View style={styles.item} />
+        <View style={styles.item} />
+        <View style={styles.item} />
+      </ScrollView>
+      <View
+        style={[
+          styles.headerImage,
+          // TODO: ヘッダー画像をアニメーションさせる
+          {
+            height: MAX_HEADER_HEIGHT,
+            zIndex: 0,
           },
-        },
-      ])}
-      scrollEventThrottle={16}
-    >
-      <View style={styles.container}>
-        <View style={{ height: 500 }} />
-        <Animated.View
-          style={{
-            width: 100,
-            height: 100,
-            margin: 10,
-            backgroundColor: 'blue',
-            opacity: animatedScrollY.interpolate({
-              inputRange: [0, 100],
-              outputRange: [1, 0],
-              extrapolate: 'clamp',
-            }),
-          }}
+        ]}
+      >
+        <Image
+          source={require('./assets/cover.jpeg')}
+          style={styles.cover}
+          resizeMethod="auto"
         />
-        <View style={{ height: 900 }} />
       </View>
-    </ScrollView>
+      <Image
+        source={require('./assets/avatar.jpeg')}
+        style={[
+          styles.avatar,
+          // TODO: アバター画像をアニメーションさせる
+          {
+            width: MAX_AVATAR_SIZE,
+            height: MAX_AVATAR_SIZE,
+            top: AVATAR_TOP,
+            zIndex: 1,
+          },
+        ]}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+  },
+  item: {
+    height: 100,
+    margin: 10,
+    backgroundColor: '#ccc',
+  },
+  cover: {
+    flex: 1,
+    width: '100%',
+  },
+  headerImage: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    width: '100%',
+    backgroundColor: 'red',
+  },
+  avatar: {
+    position: 'absolute',
+    top: AVATAR_TOP,
+    left: 20,
+    borderRadius: 50,
+    backgroundColor: '#000',
+    borderWidth: 5,
+    borderColor: '#fff',
+  },
+  blur: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    width: '100%',
+    height: MIN_HEADER_HEIGHT,
+    zIndex: 2,
+  },
+  backButton: {
+    position: 'absolute',
     justifyContent: 'center',
+    alignItems: 'center',
+    left: 10,
+    top: 35,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    width: 36,
+    height: 36,
+    borderRadius: 18,
+    zIndex: 3,
   },
 });
